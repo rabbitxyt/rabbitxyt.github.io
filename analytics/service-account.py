0bf67c58faaef399c4ace43f755e140e7223ee52
Yiting <rabbitxyt@gmail.com>
2018-06-05 23:07:43 -0700

    Demo Three

---
+++ analytics/service-account.py
@@ -0,0 +1,17 @@
+# service-account.py
+
+from oauth2client.service_account import ServiceAccountCredentials
+
+# The scope for the OAuth2 request.
+SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
+
+# The location of the key file with the key data.
+KEY_FILEPATH = 'Personal_Website.json'
+
+# Defines a method to get an access token from the ServiceAccount object.
+def get_access_token():
+  return ServiceAccountCredentials.from_json_keyfile_name(
+      KEY_FILEPATH, SCOPE).get_access_token().access_token
+
+
+print(get_access_token())
